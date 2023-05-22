620634930831d16f3345cbe71ab4ff53090e2beb
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-22 16:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function removeElement(){
+	let selectElement=document.getElementsByTagName("select")[0];
+let selectedValue=selectElement.value;
+let childrenList=selectElement.children;
+for(let i=0;i<childrenList.length;i++)
+{
+  if(selectedValue===childrenList[i].value)
+  {
+    childrenList[i].remove();
+  }
+}
+}
